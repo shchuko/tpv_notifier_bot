5af5299070a5df842b6275c62d63193b0da4c6a9
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-10-02 12:22:54 +0300

    add lambda sources and licence

---
+++ src/functions/tpv_notifier_lambda/tpv_notifier_lambda.py
@@ -0,0 +1,27 @@
+import os
+from io import BytesIO
+import requests
+
+import telebot
+
+chat_id = os.environ.get('CHAT_ID')
+token = os.environ.get('TOKEN')
+image_url = 'https://raw.githubusercontent.com/shchuko/tpv_notifier_bot/master/images/out.jpg'
+
+
+def notify(caption: str):
+    bot = telebot.TeleBot(token)
+    bot.send_chat_action(chat_id, 'upload_photo')
+    img = BytesIO(requests.get(image_url).content)
+    message = bot.send_photo(chat_id, img, caption)
+    bot.pin_chat_message(chat_id, message.message_id)
+
+
+def thursday_notifier_handler(event, context):
+    notify("Дедлайн завтра в 19.00!")
+    return {}
+
+
+def friday_notifier_handler(event, context):
+    notify("Дедлайн сегодня в 19.00!")
+    return {}
